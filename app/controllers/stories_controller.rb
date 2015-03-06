df2ebe51020302bb55fd0ccd78646986f19e8757
Todd Lynam <TLynam@gmail.com>
2015-03-05 18:16:51 -0800

    Create new story.  Show story css update

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,4 +7,21 @@ class StoriesController < ApplicationController
     @story = Story.find params[:id]
   end
 
+  def new
+    @story = Story.new user: current_user
+  end
+
+  def create
+    @story = Story.new **story_params.symbolize_keys, user: current_user
+
+    if @story.save
+      redirect_to @story
+    else
+      render 'new'
+    end
+  end
+
+  def story_params
+    params.require(:story).permit :id, :title, :writing_period, :voting_period
+  end
 end
