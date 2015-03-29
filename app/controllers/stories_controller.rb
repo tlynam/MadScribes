da380b7895429c43f92138a01291f0d60f603308
Todd Lynam <TLynam@gmail.com>
2015-03-28 21:36:18 -0700

    Homepage update

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,7 @@
 class StoriesController < ApplicationController
   def index
     @stories = Story.paginate(page: params[:page], per_page:  10).order('created_at DESC')
+    @live_stories = Story.active.paginate(page: params[:page], per_page:  10).order('created_at DESC')
   end
 
   def show
