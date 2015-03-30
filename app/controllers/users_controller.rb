8bb12b0a2c6a1e3ff42ee9b22ad40551d0478ac4
Todd Lynam <TLynam@gmail.com>
2015-03-29 22:10:33 -0700

    Permissions so users can only view their own profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
     
+    redirect_to root_path, :flash => { :notice => "You're out of your element, you can't visit other peoples' private spaces!" } unless @user == current_user
   end
-
 end
