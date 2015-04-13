4b205c841ae75de0c47080f6f5afafe9c289bfc5
Todd Lynam <TLynam@gmail.com>
2015-04-12 19:39:21 -0700

    Heroku puma and postgres setup

--- config/puma.rb
+++ config/puma.rb
@@ -9,11 +9,5 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Valid on Rails up to 4.1 the initializer method of setting `pool` size
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['DB_POOL'] || 20
-    ActiveRecord::Base.establish_connection(config)
-  end
+	ActiveRecord::Base.establish_connection
 end
