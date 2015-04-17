7954d8e1ead4cc026a225bfb40e9e90d8cff0c9c
Todd Lynam <TLynam@gmail.com>
2015-04-16 17:44:37 -0700

    Renamed connection variable to be more explanatory

--- lib/pg_notify.rb
+++ lib/pg_notify.rb
@@ -17,21 +17,20 @@ class PGNotify
         @bgthread = true
         Thread.new do
           ActiveRecord::Base.connection_pool.with_connection do |connection|
-            conn = connection.instance_variable_get(:@connection)
-
-            conn.async_exec "listen #{@channel}"
+            pg_conn = connection.instance_variable_get(:@connection)
+            pg_conn.async_exec "listen #{@channel}"
             begin
               loop do
-                conn.wait_for_notify do |channel, pid, payload|
+                pg_conn.wait_for_notify do |channel, pid, payload|
                   json = JSON.parse payload, symbolize_names: true
                   @clients.each{ |c| c.call json }
                 end
               end
             ensure
-              conn.async_exec "unlisten #{@channel}"
+              @bgthread = false
+              pg_conn.async_exec "unlisten #{@channel}"
             end
           end
-          @bgthread = false
         end
       end
     end
